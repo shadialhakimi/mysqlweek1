70d0cdd82ff0eec74317c3bee92bc01c75273952
Shadi Alhakimi <shadi_15_5@hotmail.com>
2017-02-25 14:42:58 +0100

    Home work first commit

---
+++ homework.sql
@@ -0,0 +1,8 @@
+use class6;
+#select count(*) from todos;
+#select * from todos where Done=1;
+#select * from todos where done is not true;
+#select * from todos order by id desc;
+#select * from todos order by id desc limit 1;
+select * from todos where name like '%databases%';
+ 
